3e944d90c55a3d2ebd3eae4f457b472d0cec2e8c
Jeremy Yeaton <jdyeaton27@gmail.com>
2019-10-02 12:02:51 +0200

    Update modyco_math.m

--- modyco_math.m
+++ modyco_math.m
@@ -1,3 +1,8 @@
+% Perform binnning & averaging on preprocessed data
+% (c) Jeremy Yeaton
+% Created: June 2019
+% Updated: October 2019
+
 %% Import settings from file
 mainDir      = 'C:\\Users\\jdyea\\OneDrive\\MoDyCo\\_pilotSWOP'; % Change this to your experimental directory
 cd(mainDir); 
